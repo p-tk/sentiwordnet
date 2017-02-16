88c329d551fa1df842d399dbf0d6317033e5f0b9
thejohnhoffer <hoffexi@gmail.com>
2017-02-16 09:03:09 -0500

    politick wordnet demo

--- feelings.py
+++ feelings.py
@@ -1,18 +1,40 @@
+import yaml
 import sys, argparse
 from toArgv import toArgv
+from nltk.corpus import sentiwordnet as swn
 
 def start(_argv):
     args = parseArgv(_argv)
-    return args
+    word = args['word']
+    meanings = define(word)
+    formated = map(show,meanings)
+    return yaml.dump(formated,default_flow_style=False)
+
+def define(_word):
+    return list(swn.senti_synsets(_word))
+
+def show(_meaning):
+    sdisp = lambda n: ('{0:+d}%').format(int(100*n))
+    disp = lambda n: ('{0:d}%').format(int(100*n))
+    return {
+        str(_meaning.synset.name()):
+            {
+                'fairness': disp(_meaning.obj_score()),
+                'meaning': str(_meaning.synset.definition()),
+                'feeling': sdisp(_meaning.pos_score() - _meaning.neg_score())
+            }
+    }
 
 def parseArgv(argv):
     sys.argv = argv
 
     help = {
-        'help': 'Basic demo for sentiment wordnet'
+        'help': 'Basic demo for sentiment wordnet',
+        'word': 'Word to look up all sentiments'
     }
 
     parser = argparse.ArgumentParser(description=help['help'])
+    parser.add_argument('word', help=help['word'])
 
     return vars(parser.parse_args())
 
